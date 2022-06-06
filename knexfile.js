e275f06d95811ceb9d039e65a1d0a84d36973997
White Nguyen <whitenguyen0909@gmail.com>
2022-06-06 10:50:16 -0600

    created a migration file for the suppliers table

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+const path = require("path"); //specifying a storage location for migration
 require("dotenv").config();
 const { DATABASE_URL } = process.env;
 
@@ -5,5 +6,10 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
   },
 };
+//migrations must be plural
+//must be inside the development configuration object
